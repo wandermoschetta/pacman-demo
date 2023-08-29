53239ae4549b8808c79489fa9557bc8d63f2c050
Wander S. Moschetta <wander.sm@gmail.com>
2023-08-28 23:58:19 -0300

    setup CMake file

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Window.hpp>
+
+int main()
+{
+    sf::Window window;
+    window.create(sf::VideoMode(800, 600), "Pacman Demo"); 
+    
+    while(window.isOpen())
+    {
+        sf::Event event;
+
+        while(window.pollEvent(event))
+        {
+
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+    }
+    
+    return 0;
+}
+
